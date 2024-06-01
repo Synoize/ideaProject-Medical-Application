cb541cf9f52eeb6a339865ea7c9178584fd975d7
Shivam Rajput <suryavanshivam@gmail.com>
2024-06-01 11:35:58 +0530

    prototype

---
+++ script.js
@@ -0,0 +1,31 @@
+function searchBtn(){
+    var search = document.getElementById("search_dd");
+    search.style="display:block; transition:0.8s";
+   
+}
+
+function messages(){
+    alert("Now You have not any Messages :(");
+}
+
+function notifications(){
+    alert("Now You have not any Notification :(");
+}
+
+
+
+function open_ddMenu(){
+    var open = document.getElementById("mobile_ddMenu");
+    open.style="display:block; transition:0.8s";
+   
+}
+
+
+function close_dd(){
+    var close = document.getElementById("mobile_ddMenu");
+    close.style="display:hidden; transition:0.8s";
+   
+}
+
+
+

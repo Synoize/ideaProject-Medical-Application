f14d356a4565c86c047c859aee1fe888e9cf8f5e
Shivam Rajput <suryavanshivam@gmail.com>
2024-07-01 15:02:15 +0530

    0.0.1

--- script.js
+++ script.js
@@ -29,3 +29,12 @@ function close_dd(){
 
 
 
+let loader = document.querySelector('#preloader');
+let header = document.querySelector('header');
+header.style.visibility='hidden'
+
+window.addEventListener('load',function(){
+    header.style.visibility='visible'
+    loader.style.display = 'none'
+
+})
